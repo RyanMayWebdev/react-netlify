d491377f4356249a4d3a17b9ebd9a4c449e67ae5
Ryan May <ryan777may@gmail.com>
2021-10-04 12:40:58 -0400

    Added some text

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>It's a React app on Netlify, what else do you want from me?</h1>
+        <p>Wow a paragraph!</p>
+        <ul>
+          <li>Much list, many words</li>
+          <li>Much list, many words</li>
+          <li>Much list, many words</li>
+          <li>Much list, many words</li>
+          <li>Much list, many words</li>
+          <li>Much list, many words</li>
+          <li>Much list, many words</li>
+        </ul>
     </div>
   );
 }
