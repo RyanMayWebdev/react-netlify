5d0dd18df1ed8785ac995832483acfc5331a40e7
Ryan May <ryan777may@gmail.com>
2021-10-04 12:43:03 -0400

    Added some styling

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <h1>It's a React app on Netlify, what else do you want from me?</h1>
-        <p>Wow a paragraph!</p>
-        <ul>
-          <li>Much list, many words</li>
-          <li>Much list, many words</li>
-          <li>Much list, many words</li>
-          <li>Much list, many words</li>
-          <li>Much list, many words</li>
-          <li>Much list, many words</li>
-          <li>Much list, many words</li>
-        </ul>
+<div className="wrapper">
+          <h1>It's a React app on Netlify, what else do you want from me?</h1>
+          <p>Wow a paragraph!</p>
+          <ul>
+            <li>Much list, many words</li>
+            <li>Much list, many words</li>
+            <li>Much list, many words</li>
+            <li>Much list, many words</li>
+            <li>Much list, many words</li>
+            <li>Much list, many words</li>
+            <li>Much list, many words</li>
+          </ul>
+</div>
     </div>
   );
 }
