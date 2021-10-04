79cc5cf4815d801a6053ebc598f53a7827e3aa05
Ryan May <ryan777may@gmail.com>
2021-10-04 13:09:06 -0400

    added delays to text

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
+import { useState } from 'react';
 import './App.css';
+import FirstParagraph from './FirstParagraph';
+
+
 
 function App() {
+
+  const [showOne, setShowOne] = useState(false)
+const [showTwo, setShowTwo] = useState(false)
+const [showThree, setShowThree] = useState(false)
+
+const delayShow = () => {
+  console.log('running')
+  setTimeout( () => {setShowOne(true)}, 2000);
+  setTimeout( () => setShowTwo(true), 4000);
+  setTimeout( () => setShowThree(true), 6000);
+}
+
+delayShow();
     return (
         <div className="App">
             <div className="wrapper">
                 <p>Wake up, Neo...</p>
-                <p>
-                    The Matrix has you...</p>
-                <p>
-                    Follow the white rabbit.</p>
+                { showOne ? <FirstParagraph /> : null}
+                    {showTwo ? <p> Follow the white rabbit.</p> : null}
 
-                <p>Knock, Knock, Neo.</p>
+                {showThree? <p className="knockknock">Knock, Knock, Neo.</p> : null}
             </div>
         </div>
     );
---
+++ src/FirstParagraph.js
@@ -0,0 +1,8 @@
+
+const FirstParagraph = () => {
+
+        return (
+          <p> The Matrix has you...</p>)
+}
+
+export default FirstParagraph;
